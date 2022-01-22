408a9b0596bfee5f2b248b19c66764ffd07a5412
maxime.dubourg <maxime.dubourg@ekino.com>
2022-01-22 21:36:25 +0100

    wip

--- examples/jest-replacement/setupTests.ts
+++ examples/jest-replacement/setupTests.ts
@@ -1,3 +1,24 @@
-import { GlobalRegistrator } from "@happy-dom/global-registrator";
+import { parseHTML } from "linkedom";
 
-GlobalRegistrator.register();
+const defaultHtml =
+  '<!doctype html><html><head><meta charset="utf-8"></head><body></body></html>';
+
+const dom = parseHTML(defaultHtml);
+const { window } = dom;
+const { document } = window;
+
+const KEYS = [];
+
+if (KEYS.length === 0) {
+  KEYS.push(
+    ...Object.getOwnPropertyNames(window)
+      .filter((k) => !k.startsWith("_"))
+      .filter((k) => !(k in global))
+  );
+}
+
+KEYS.forEach((key) => (global[key] = window[key]));
+
+global.document = document;
+global.window = window;
+window.console = global.console;
